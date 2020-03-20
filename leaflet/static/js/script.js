f40adcb2b2981649d024de1da039d3f779600a41
ronyonka <rononyonka@gmail.com>
2020-03-20 18:03:42 +0300

    removed secret keys

--- leaflet/static/js/script.js
+++ leaflet/static/js/script.js
@@ -18,7 +18,7 @@ var mymap = L.map('mapid').setView([0, 0], 2.6);
 covidCases()
 // var covid = axios.get()
 // console.log(covid)
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicm9ueW9ua2EiLCJhIjoiY2sxbTF1cjlrMDdkdTNpcDZyNmQ3eG9xeCJ9.GwSPf8q-E9jtvrRrVPgksg', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=access_token', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     minZoom: 2,
     maxZoom: 18,
