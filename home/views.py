e0d735f0ccb630b0d87f7996cdc7498c2aa6885a
ronyonka <rononyonka@gmail.com>
2020-03-21 12:32:40 +0300

    added secret access token

--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from leaflet.settings import ACCESS_TOKEN
 
 def home(request):
-    return render(request, 'index.html')
+    mapbox_access_token = ACCESS_TOKEN
+    context = {
+        'access_token':mapbox_access_token
+    }
+    return render(request, 'index.html', context)
--- leaflet/settings.py
+++ leaflet/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import dj_database_url
 import django_heroku
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+ACCESS_TOKEN=config('ACCESS_TOKEN')
