ae4b58566f3e1981024ce81780943ec28997fc81
ronyonka <rononyonka@gmail.com>
2020-03-22 16:08:59 +0300

    added dynamic point sizing

--- leaflet/static/js/script.js
+++ leaflet/static/js/script.js
@@ -8,7 +8,7 @@ function covidCases() {
           //     let cases = response.data[i].cases
           //     console.log(`${country} has recorded ${cases} cases`)
           // }
-        console.log(response.data)
+        // console.log(response.data)
       })
       .catch(function (error) {
         console.log(error)
@@ -30,13 +30,26 @@ L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 for(let j=0; j<covid_cases.length; j++){
 if(covid_cases[j]['coordinates']){
-  var circle = L.circle(covid_cases[j]['coordinates'], {
+  let country = covid_cases[j]['country']
+  let cases = covid_cases[j]['cases']
+  let active_cases = covid_cases[j]['active']
+  let deaths = covid_cases[j]['deaths']
+  let recovered = covid_cases[j]['recovered']
+  let point = 500
+  if(cases>100){
+    point = (cases/100)*500
+  }
+  let circle = L.circle(covid_cases[j]['coordinates'], {
     color: 'red',
     fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
+    fillOpacity: 0.3,
+    radius: point
   }).addTo(mymap);
+  // let marker = L.marker(covid_cases[j]['coordinates']).addTo(mymap);
+  // marker.bindPopup("I am a circle.");
+}else{
+  console.log(covid_cases[j]['country']+" does not have coordinates")
 }
 }
 
-console.log(covid_cases)
+// console.log(covid_cases)
